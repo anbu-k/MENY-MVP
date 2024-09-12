e452b5e9f9a58415e314c1fdb0d479d28b5780f9
Isaac Meyers <IMeyers701@gmail.com>
2024-09-12 16:16:24 -0500

    feat: add test data for layers

--- MapstructorApp/meny_react/app/page.tsx
+++ MapstructorApp/meny_react/app/page.tsx
@@ -4,9 +4,130 @@ import SectionLayersComponent from "./components/layers/section-layers.component
 import moment from 'moment';
 import { useState } from "react";
 import SliderWithDatePanel from "./components/slider/slider-with-date-panel.component";
+import { SectionLayer } from "./models/layers/layer.model";
+
+// Remove this when we have a way to get layers correctly
+const manhattanLayerSections = [
+  {
+    id: "grants_layer_items",
+    caretId: "dutch-grants-layer-caret",
+    label: "1640-64 | Dutch Grants",
+    itemSelector: ".dutch_grants_layer_item",
+    zoomTo: "NA",
+    infoId: "grants-info-layer",
+    type: "group",
+  },
+  {
+    id: "grants_layer",
+    className: "grants_layer",
+    name: "grants_layer",
+    iconColor: "#e3ed58",
+    label: "Information",
+    topLayerClass: "dutch_grants_layer",
+    iconType: "square",
+    isSolid: true,
+  },
+  {
+    id: "grants_layer_lines",
+    className: "grants_layer",
+    name: "grants_layer_lines",
+    iconColor: "#ff0000",
+    label: "Lines",
+    topLayerClass: "dutch_grants_layer",
+    iconType: "square",
+    isSolid: false,
+  },
+  {
+    id: "circle_point",
+    name: "circle_point",
+    checked: true,
+    label: "1643-75 | Lot Events",
+    iconColor: "#097911",
+    zoomTo: "NA",
+    infoId: "demo-taxlot-info-layer",
+    type: "lots-events",
+  },
+  {
+    id: "castello_points",
+    name: "castello_points",
+    label: "1660 | Castello Taxlots",
+    iconColor: "#ff0000",
+    zoomTo: "NA",
+    infoId: "castello-info-layer",
+    type: "castello-points",
+  },
+];
+
+// Remove this when we have a way to get layers correctly
+const longIslandLayerSections = [
+  {
+    id: "native_groups_layer_items",
+    name: "native_groups_layer_items",
+    caretId: "native-groups-layer-caret",
+    label: "1600s | Long Island Tribes",
+    zoomTo: "LongIsland",
+    infoId: "native-groups-info-layer",
+    type: "group",
+    itemSelector: ".native_groups_layer_item",
+  },
+  {
+    className: "native_groups_layer",
+    id: "native_groups_labels",
+    name: "native_groups_labels",
+    iconColor: "#0b0ee5",
+    label: "Labels",
+    topLayerClass: "native_groups_layer",
+    iconType: "comment-dots",
+    isSolid: true,
+  },
+  {
+    className: "native_groups_layer",
+    id: "native_groups_area",
+    name: "native_groups_area",
+    iconColor: "#ff1493",
+    label: "Area",
+    iconType: "square",
+    topLayerClass: "native_groups_layer",
+    isSolid: true,
+  },
+  {
+    className: "native_groups_layer",
+    id: "native_groups_lines",
+    name: "native_groups_lines",
+    iconColor: "#ff0000",
+    label: "Borders",
+    iconType: "square",
+    topLayerClass: "native_groups_layer",
+  }
+];
 
 export default function Home() {
   const [currDate, setCurrDate] = useState<moment.Moment | null>(null)
+
+  const currLayers: SectionLayer[] = [
+    ...manhattanLayerSections.map(x => {
+      let mappedVal: SectionLayer = {
+        id: x.id,
+        name: x.name ?? "",
+        label: x.label,
+        checked: x.checked ?? false,
+        iconColor: x.iconColor ?? ""
+      }
+
+      return mappedVal
+    }),
+    ...longIslandLayerSections.map(x => {
+      let mappedVal: SectionLayer = {
+        id: x.id,
+        name: x.name ?? "",
+        label: x.label,
+        checked: false,
+        iconColor: x.iconColor ?? ""
+      }
+
+      return mappedVal;
+    })
+  ];
 
   return (
     <>
@@ -104,7 +225,7 @@ export default function Home() {
       </div>
 
       {/* Add Layers in here when structured somewhere */}
-      <SectionLayersComponent layers={[]} />
+      <SectionLayersComponent layers={currLayers} />
 
       <div id="before" className="map"></div>
       <div id="after" className="map"></div>
