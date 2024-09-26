f79e440d104a04024eebcaa61caa105ef5f01589
Haakon H <hhaus532@iastate.edu>
2024-09-25 22:13:25 -0500

    Detele add to API

--- MapstructorApp/meny_react/app/api/map/route.ts
+++ MapstructorApp/meny_react/app/api/map/route.ts
@@ -3,20 +3,11 @@ import { NextResponse } from 'next/server';
 import { Map } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
-// export  async function GET(request: Request)
-// {
-
-//     const m : Map = await prisma.map.findMany({ select: { id: true } }); //find map
-
-//     return NextResponse.json({ //return map
-//         m
-//      })
-// }
+//TODO - make a blank-all functions
 
 export  async function GET()
 {
-
+    console.log("GET ALL REQUSET:");
     const m : Map = await prisma.map.findMany(); //find all maps
 
     return NextResponse.json({ //return maps
@@ -24,12 +15,24 @@ export  async function GET()
      })
 }
 
-export async function POST(request: Request) {
+// export  async function GETALL()
+// {
+//     console.log("GET ALL REQUSET:");
+//     const m : Map = await prisma.map.findMany(); //find all maps
+
+//     return NextResponse.json({ //return maps
+//         m
+//      })
+// }
+
+export async function POST(request: Request) { //create
     try {
         const m: Map = await request.json(); //parsars the JSON
 
         //console.log("Request Headers:", request.headers);  //DEBUG
         //console.log("Incoming data:", m);
+
+        console.log("POST REQUSET:", m);
 
         if (!m.id || !m.name || m.checked === undefined || !m.infoId || !m.zoomFunction) { //check to see if the JSON is vaild
             console.error("Validation error: Missing required fields", m);
@@ -68,11 +71,42 @@ export async function POST(request: Request) {
     }
 }
 
-export async function DELETE(request: Request){
+export async function DELETE(request: Request){ //delete 
+    const m: Map = await request.json();
 
+    console.log("DELETE REQUSET:", m.id);//logging
+
+    
+    try{
+        if (!m.id || !m.name || m.checked === undefined || !m.infoId || !m.zoomFunction) { //check to see if the JSON is vaild
+            console.error("Validation error: Missing required fields", m);
+            return NextResponse.json({ //sends error and 400 (Bad Request) if not
+                message: "Invalid data: ",
+                error: "Missing required fields",
+            }, { status: 400 });
+        }
+
+        const delmap = await prisma.map.delete({ //find and delete the map id that was sent
+            where: {
+              id: m.id,
+            },
+          })
+
+        return NextResponse.json({ //send success and the delmap data back 
+            message: "Success",
+            data: delmap
+        });
+    }
+    catch (error: any) { //catch error if try fails
+        console.error("Error creating map:", error); //log to server
+        return NextResponse.json({ //send 500 (Internal Server Error) and what the error is to frontend
+            message: "Failed to create map",
+            error: error.message,
+        }, { status: 500 });
+    } 
 }
 
-export async function PUT(request: Request){
+export async function PUT(request: Request){ //create or modify
 
 }
 
