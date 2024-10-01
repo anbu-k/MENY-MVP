796977617a24a984ba8e54f37cf3135d6aa4faa9
Haakon H <hhaus532@iastate.edu>
2024-10-01 17:54:24 -0500

    basic untested service is done

--- MapstructorApp/meny_react/app/components/service/MapService.tsx
+++ MapstructorApp/meny_react/app/components/service/MapService.tsx
@@ -1,10 +1,40 @@
-import axios from 'axios';
+import api from './service';
 
-const fetchData = async () => {
-    try {
-        const response = await axios.get('/api/');
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
+export interface Map {
+  id:                 Number;
+  name:               String;
+  longitude:          Number;
+  latitude:           Number;
+  infoId:             String;
+  zoom:               Number;
+  bearing:            Number;
+  attributionControl: Boolean;
+}
+
+const MapService = {
+  getMaps: async (): Promise<Map[]> => {
+    const response = await api.get<Map[]>('/api/map');
+    return response.data;
+  },
+
+  getMapById: async (id: number): Promise<Map> => {
+    const response = await api.get<Map>(`/api/map/${id}`);
+    return response.data;
+  },
+
+  createMap: async (Map: Omit<Map, 'id'>): Promise<Map> => {
+    const response = await api.post<Map>('/api/map', Map);
+    return response.data;
+  },
+
+  updateMap: async (Map: Map): Promise<Map> => {
+    const response = await api.put<Map>(`/api/map/${Map.id}`, Map);
+    return response.data;
+  },
+
+  deleteMap: async (id: number): Promise<void> => {
+    await api.delete(`/api/map/${id}`);
+  },
 };
+
+export default MapService;
