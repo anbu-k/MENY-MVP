3128919a73a1034e942a8f4e83a546421a66d52f
Haakon H <hhaus532@iastate.edu>
2024-10-02 21:02:23 -0500

    added error handler

--- MapstructorApp/meny_react/app/components/service/MapService.tsx
+++ MapstructorApp/meny_react/app/components/service/MapService.tsx
@@ -12,28 +12,58 @@ export interface Map {
 }
 
 const MapService = {
-  getMaps: async (): Promise<Map[]> => {
-    const response = await api.get<Map[]>('/api/map');
-    return response.data;
+  getMaps: async (): Promise<Map[] | null> => {
+    try{
+      const response = await api.get<Map[]>('/api/map');
+      return response.data;
+    }
+    catch(error){
+      console.log("Error: getmaps request", error);
+      return null;
+    }
+
   },
 
-  getMapById: async (id: number): Promise<Map> => {
+  getMapById: async (id: number): Promise<Map | null> => {
+  try{
     const response = await api.get<Map>(`/api/map/${id}`);
     return response.data;
+  }
+  catch(error){
+    console.log("Error: getmapbyId request", error);
+    return null;
+  }
   },
 
-  createMap: async (Map: Omit<Map, 'id'>): Promise<Map> => {
+  createMap: async (Map: Omit<Map, 'id'>): Promise<Map | null> => {
+  try{
     const response = await api.post<Map>('/api/map', Map);
     return response.data;
+  }
+  catch(error){
+    console.log("Error: create map request", error);
+    return null;
+  }
   },
 
-  updateMap: async (Map: Map): Promise<Map> => {
+  updateMap: async (Map: Map): Promise<Map | null> => {
+    try{
     const response = await api.put<Map>(`/api/map/${Map.id}`, Map);
     return response.data;
+  }
+  catch(error){
+    console.log("Error: update map request", error);
+    return null;
+  }
   },
 
   deleteMap: async (id: number): Promise<void> => {
+  try{
     await api.delete(`/api/map/${id}`);
+  }
+  catch(error){
+    console.log("Error: delete request", error);
+  }
   },
 };
 
