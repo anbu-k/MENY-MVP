ae5c13a0202c154de8912fca9d45bbc880a9231a
AbhiMeta <abhimehta0314@gmail.com>
2024-10-03 11:51:28 -0500

    Code now uses MongoDB

--- MapstructorApp/meny_react/app/api/map/route.ts
+++ MapstructorApp/meny_react/app/api/map/route.ts
@@ -1,16 +1,33 @@
 import { NextApiRequest, NextApiHandler, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+import {Map}  from "@prisma/client"
+import { NextResponse } from "next/server";
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-)
-{
+
+export async function GET() {
     const prisma = new PrismaClient();
-    if(req.method === 'GET') {
-        const map = await prisma.map.findMany();
-        return res.send(map)
-    }
-    const map = await prisma.map.findMany();
-        return res.send(map)
+    const maps = (await prisma.map.findMany());
+
+    return NextResponse.json({
+        maps
+    })
+}
+
+
+export async function POST(request: Request) {
+    const map:Map = await request.json()
+    const prisma = new PrismaClient();
+    await prisma.map.create({
+        data: {
+            mapId: map.mapId,
+            name: map.name,
+            checked: map.checked,
+            infoId: map.infoId,
+            zoomFunction: map.zoomFunction
+        }
+
+    })
+    return NextResponse.json({
+        message: "Success"
+    })
 }
